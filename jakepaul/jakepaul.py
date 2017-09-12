73cb294adfe086e2c55e973e06b51349529a017c
DavidTechnoboy <DavidTechnoboy@users.noreply.github.com>
2017-09-11 22:36:31 -0400

    added new commands

--- jakepaul/jakepaul.py
+++ jakepaul/jakepaul.py
@@ -11,6 +11,21 @@ class jakepaul:
     async def dabonhaters(self, user : discord.Member):
         
         await self.bot.say("" + user.mention + " DO YOU DAB ON THEM HATERS? ( ͡° ͜ʖ ͡°) https://imgur.com/a/CKsco")
+
+    @commands.command()
+    async def itseverydaybro(self, user : discord.Member):
+        
+        await self.bot.say("" + user.mention + " ITS EVERYDAY BRO! ( ͡° ͜ʖ ͡°) https://www.youtube.com/watch?v=hSlb1ezRqfA")
 		
+    @commands.command()
+    async def fuckjakepaul (self, user : discord.Member):
+        
+        await self.bot.say("" + user.mention + " FAKE WIFE! FAKE LIFE! ( ͡° ͜ʖ ͡°) https://www.youtube.com/watch?v=tCMx_RPLCYc")
+		
+    @commands.command()
+    async def jakepaulthemesong (self, user : discord.Member):
+        
+        await self.bot.say("" + user.mention + " GOT EVERYDAY BRO ON MY FLIP FLOPS! ( ͡° ͜ʖ ͡°) https://www.youtube.com/watch?v=D7_9b20qs1w")		
+
 def setup(bot):
     bot.add_cog(jakepaul(bot))
