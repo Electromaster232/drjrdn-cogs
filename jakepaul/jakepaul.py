1224a117baf5ffc7581b3c2334338654157810b3
DavidTechnoboy <DavidTechnoboy@users.noreply.github.com>
2017-09-15 15:54:25 -0400

    full redo

--- jakepaul/jakepaul.py
+++ jakepaul/jakepaul.py
@@ -8,44 +8,49 @@ class jakepaul:
         self.bot = bot
 
     @commands.command()
-    async def dabonhaters(self, user : discord.Member):
+    async def itseverydaybro (self):
         
-        await self.bot.say("" + user.mention + " DO YOU DAB ON THEM HATERS? ( ͡° ͜ʖ ͡°) https://imgur.com/a/CKsco")
-
-    @commands.command()
-    async def itseverydaybro(self, user : discord.Member):
-        
-        await self.bot.say("" + user.mention + " ITS EVERYDAY BRO! ( ͡° ͜ʖ ͡°) https://www.youtube.com/watch?v=hSlb1ezRqfA")
+        await self.bot.say("ITS EVERYDAY BRO! ( ͡° ͜ʖ ͡°) https://www.youtube.com/watch?v=hSlb1ezRqfA")
 		
     @commands.command()
-    async def everydaybroparody (self, user : discord.Member):
+    async def itseverydaybroparody (self):
         
-        await self.bot.say("" + user.mention + " ZERO TALENT CREW! ( ͡° ͜ʖ ͡°) https://www.youtube.com/watch?v=c3wuf9_ntgU")
+        await self.bot.say("THE ZERO TALENT CREW! ( ͡° ͜ʖ ͡°) https://www.youtube.com/watch?v=c3wuf9_ntgU")
 		
     @commands.command()
-    async def fuckjakepaul (self, user : discord.Member):
+    async def assulted (self):
         
-        await self.bot.say("" + user.mention + " FAKE WIFE! FAKE LIFE! ( ͡° ͜ʖ ͡°) https://www.youtube.com/watch?v=tCMx_RPLCYc")
+        await self.bot.say("it was more extreme ( ͡° ͜ʖ ͡°) https://youtu.be/DUiD7j6DdVQ?t=12m57s")
 		
     @commands.command()
-    async def reactfuckjakepaul (self, user : discord.Member):
+    async def assultedparody (self):
         
-        await self.bot.say("" + user.mention + " HACKED BY BANKS! ( ͡° ͜ʖ ͡°) https://www.youtube.com/watch?v=5RoNgAVP3K4")
+        await self.bot.say("it was more extreme ( ͡° ͜ʖ ͡°) https://youtu.be/txl3ijxkhu4?t=15s")
 		
     @commands.command()
-    async def jakepaulthemesong (self, user : discord.Member):
+    async def fuckjakepaul (self):
         
-        await self.bot.say("" + user.mention + " JAKE PAULERS KEEP IT LIT! ( ͡° ͜ʖ ͡°) https://www.youtube.com/watch?v=D7_9b20qs1w")
+        await self.bot.say("FAKE WIFE! FAKE LIFE! ( ͡° ͜ʖ ͡°) https://www.youtube.com/watch?v=tCMx_RPLCYc")
 		
     @commands.command()
-    async def assulted (self, user : discord.Member):
+    async def fuckjakepaulreact (self):
         
-        await self.bot.say("" + user.mention + " it was more extreme ( ͡° ͜ʖ ͡°) https://youtu.be/DUiD7j6DdVQ?t=12m57s")
+        await self.bot.say("HACKED BY BANKS! ( ͡° ͜ʖ ͡°) https://www.youtube.com/watch?v=5RoNgAVP3K4")
 		
     @commands.command()
-    async def assultedparody (self, user : discord.Member):
+    async def jakepaulthemesong (self):
         
-        await self.bot.say("" + user.mention + " it was more extreme ( ͡° ͜ʖ ͡°) https://youtu.be/txl3ijxkhu4?t=15s")
+        await self.bot.say("JAKE PAULERS KEEP IT LIT! ( ͡° ͜ʖ ͡°) https://www.youtube.com/watch?v=D7_9b20qs1w")
+		
+    @commands.command()
+    async def dabonhaters (self):
+        
+        await self.bot.say("DO YOU DAB ON THEM HATERS? ( ͡° ͜ʖ ͡°) https://imgur.com/a/CKsco")
+		
+    @commands.command()
+    async def praytogod (self):
+        
+        await self.bot.say(":pray: https://i.imgur.com/Fndgpwy.png")
 
 def setup(bot):
     bot.add_cog(jakepaul(bot))
